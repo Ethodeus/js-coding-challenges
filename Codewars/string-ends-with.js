0943bdeb932c144da626d7144e077d7f54ae9a9a
Sebastian Ospina <sc.ospc@gmail.com>
2022-03-23 00:11:32 -0500

    4 new code challenges

---
+++ Codewars/string-ends-with.js
@@ -0,0 +1,18 @@
+//TODO Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+// function solution(str, ending){
+//   if (str.endsWith(ending)) {
+//     return true
+//   } else {
+//     return false
+//   }
+// }
+
+let solution = (a,b) => a.endsWith(b) ? true : false
+
+
+array.forEach(element => {
+    
+});
+
+
