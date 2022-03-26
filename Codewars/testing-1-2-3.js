f33bbec1d21eff6498be920e5cc94be8f95a23ce
Sebastian Ospina <sc.ospc@gmail.com>
2022-03-26 12:53:01 -0500

    coding challenge

--- Codewars/testing-1-2-3.js
+++ Codewars/testing-1-2-3.js
@@ -5,3 +5,11 @@
 // The numbering starts at 1. The format is n: string. Notice the colon and space in between.
 
 const number = (array) => array.map((e, i) => `${i + 1}: ${e}`);
+
+console.log(number(['', '', '', '', '']));
+
+const number2 = (arr) => arr.map((e, i) => `${arr.indexOf(e, i) + 1}: ${e}`); //this solution is redundant sinbce you are already getting the index of all the elements of the original array with the map method.
+
+console.log(number2(['', '', '', '', '']));
+
+//Also to keep in mind, The indexOf() method returns the first index at which a given element can be found in the array, or -1 if it is not present.
