706536ae3f82caf5b567fb1b9576767c8abbf8de
Sebastian Ospina <sc.ospc@gmail.com>
2022-03-28 20:19:34 -0500

    daily coding challenges

---
+++ Codewars/beginner.-series-school-paperwork.js
@@ -0,0 +1,3 @@
+//TODO: Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.
+
+let paperwork = (n, m) => n < 0 || m < 0 ? 0 : n*m
