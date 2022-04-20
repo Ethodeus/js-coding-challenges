3eba434e73fc3e4299a1e56663fea74e3cb0d117
Sebastian Ospina <sc.ospc@gmail.com>
2022-04-19 20:27:38 -0500

    daily code challenge 7

---
+++ Codewars/8kyu/8-grasshopper--messi-goals.js
@@ -0,0 +1,36 @@
+//URL -> https://www.codewars.com/kata/55ca77fa094a2af31f00002a
+
+//* NAME -> Grasshopper - Messi Goals
+
+//TODO INSTRUCTIONS ->
+
+/*
+Messi's Goal Total
+Use variables to find the sum of the goals Messi scored in 3 competitions
+
+Information
+Messi goal scoring statistics:
+
+Competition	Goals
+La Liga	43
+Champions League	10
+Copa del Rey	5
+Task
+Create these three variables and store the appropriate values using the table above:
+laLigaGoals
+championsLeagueGoals
+copaDelReyGoals
+Create a fourth variable named totalGoals that stores the sum of all of Messi's goals for this year.
+*/
+
+//SOLUTION(S) ->
+
+var laLigaGoals = 43;
+var championsLeagueGoals = 10;
+var copaDelReyGoals = 5;
+
+var totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals;
+
+//TEST CASES ->
+
+console.log(totalGoals); // -> 58
