28f9110d6b4951fd25a75a839eb81f9f79b32403
Sebastian Ospina <sc.ospc@gmail.com>
2022-04-20 23:13:51 -0500

    daily coding challenge 4

---
+++ Codewars/8kyu/8-switch-it-up.js
@@ -0,0 +1,59 @@
+//URL -> https://www.codewars.com/kata/5808dcb8f0ed42ae34000031
+
+//* NAME -> Switch it Up!
+
+//TODO INSTRUCTIONS -> When provided with a number between 0-9, return it in words.
+
+/*
+Input :: 1
+
+Output :: "One".
+
+If your language supports it, try using a switch statement.
+*/
+
+//SOLUTION(S) ->
+function switchItUp(number) {
+	switch (number) {
+		case 0:
+			return 'Zero';
+			break;
+		case 1:
+			return 'One';
+			break;
+		case 2:
+			return 'Two';
+			break;
+		case 3:
+			return 'Three';
+			break;
+		case 4:
+			return 'Four';
+			break;
+		case 5:
+			return 'Five';
+			break;
+		case 6:
+			return 'Six';
+			break;
+		case 7:
+			return 'Seven';
+			break;
+		case 8:
+			return 'Eight';
+			break;
+		case 9:
+			return 'Nine';
+			break;
+	}
+}
+
+//Clever alternative
+
+switchItUp = (n) => ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'][n];
+
+//TEST CASES ->
+
+console.log(switchItUp(5)); // -> Five
+console.log(switchItUp(7)); // ->   Seven
+console.log(switchItUp(8)); // ->   Eight
