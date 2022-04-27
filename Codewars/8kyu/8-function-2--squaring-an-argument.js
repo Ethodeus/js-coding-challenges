c3aecafaf9ab29374e9898e79d88cae53260a7a4
Sebastian Ospina <sc.ospc@gmail.com>
2022-04-27 12:13:55 -0500

    daily coding challenge 1

---
+++ Codewars/8kyu/8-function-2--squaring-an-argument.js
@@ -0,0 +1,15 @@
+//URL -> https://www.codewars.com/kata/523b623152af8a30c6000027
+
+//* NAME -> Function 2 - squaring an argument
+
+//TODO INSTRUCTIONS -> Now you have to write a function that takes an argument and returns the square of it.
+
+//SOLUTION(S) ->
+
+const square = (a) => Math.pow(a, 2);
+
+//TEST CASES ->
+
+console.log(square(5)); // -> 25
+console.log(square(10)); // -> 100
+console.log(square(8)); // -> 64
