cea861b2cff0aaecb0891dbaea5fec216e03e5a2
Sebastian Ospina <sc.ospc@gmail.com>
2022-04-30 23:27:26 -0500

    daily coding challenge 2

--- Codewars/8kyu/8-correct-the-mistakes-of-the-character-recognition-software.js
+++ Codewars/8kyu/8-correct-the-mistakes-of-the-character-recognition-software.js
@@ -36,8 +36,12 @@ function correct(string) {
 	return string;
 }
 
+//Codewars Alternatives
+
 //TEST CASES ->
 
-console.log(); // ->
-console.log(); // ->
-console.log(); // ->
+console.log(correct('L0ND0N')); // -> LONDON
+console.log(correct('DUBL1N')); // -> DUBLIN
+console.log(correct('51NGAP0RE')); // -> SINGAPORE
+console.log(correct('BUDAPE5T')); // -> BUDAPEST
+console.log(correct('PAR15')); // -> PARIS
---
+++ Codewars/8kyu/8-is-it-even.js
@@ -0,0 +1,35 @@
+//URL -> https://www.codewars.com/kata/555a67db74814aa4ee0001b5
+
+//* NAME -> Is it even?
+
+//TODO INSTRUCTIONS ->
+
+/*
+In this Kata we are passing a number (n) into a function.
+
+Your code will determine if the number passed is even (or not).
+
+The function needs to return either a true or false.
+
+Numbers may be positive or negative, integers or floats.
+
+Floats with decimal part non equal to zero are considered UNeven for this kata.
+*/
+
+//SOLUTION(S) ->
+
+function testEven(n) {
+	return n % 2 === 0;
+}
+
+//Arrow function alternative
+
+testEven = (n) => n % 2 === 0;
+
+//TEST CASES ->
+
+console.log(testEven(0), true, 'testEven for 0');
+console.log(testEven(0.5), false, 'testEven for 0.5');
+console.log(testEven(1), false, 'testEven for 1');
+console.log(testEven(2), true, 'testEven for 2');
+console.log(testEven(-4), true, 'testEven for 2');
