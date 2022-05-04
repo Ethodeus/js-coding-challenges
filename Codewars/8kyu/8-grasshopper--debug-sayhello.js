df69af469f94a0b7686016df58646891b66bc16f
Sebastian Ospina <sc.ospc@gmail.com>
2022-05-03 21:04:20 -0500

    daily coding challenge 3

---
+++ Codewars/8kyu/8-grasshopper--debug-sayhello.js
@@ -0,0 +1,24 @@
+//URL -> https://www.codewars.com/kata/5625618b1fe21ab49f00001f
+
+//* NAME -> Grasshopper - Debug sayHello
+
+//TODO INSTRUCTIONS -> The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+
+/*
+Example output:
+
+Hello, Mr. Spock
+*/
+
+//SOLUTION(S) ->
+
+function sayHello(name) {
+	return `Hello, ${name}`;
+}
+
+//TEST CASES ->
+
+console.log(sayHello('Mr. Spock'), 'Hello, Mr. Spock');
+console.log(sayHello('Captain Kirk'), 'Hello, Captain Kirk');
+console.log(sayHello('Liutenant Uhura'), 'Hello, Liutenant Uhura');
+console.log(sayHello('Dr. McCoy'), 'Hello, Dr. McCoy');
