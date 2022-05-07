e07038fc4dc6f83a50755fc4cf3822271de14066
Sebastian Ospina <sc.ospc@gmail.com>
2022-05-06 22:55:22 -0500

    daily coding challenge 3

---
+++ Codewars/8kyu/8-reversed-words.js
@@ -0,0 +1,27 @@
+//URL -> https://www.codewars.com/kata/51c8991dee245d7ddf00000e
+
+//* NAME -> Reversed Words
+
+//TODO INSTRUCTIONS ->
+
+/*
+Complete the solution so that it reverses all of the words within the string passed in.
+
+Example:
+
+"The greatest victory is that which requires no battle" --> "battle no requires which that is victory greatest The"
+*/
+
+//SOLUTION(S) ->
+
+function reverseWords(str) {
+	return str.split(' ').reverse().join(' ');
+}
+
+//TEST CASES ->
+
+console.log(reverseWords('hello world!'), 'world! hello');
+console.log(reverseWords("yoda doesn't speak like this"), "this like speak doesn't yoda");
+console.log(reverseWords('foobar'), 'foobar');
+console.log(reverseWords('kata editor'), 'editor kata');
+console.log(reverseWords('row row row your boat'), 'boat your row row row');
