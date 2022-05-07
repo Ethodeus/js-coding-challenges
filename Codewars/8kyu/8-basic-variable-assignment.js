ed5c93b5db1a816781ed14dd1de860e70088e46c
Sebastian Ospina <sc.ospc@gmail.com>
2022-05-06 23:03:57 -0500

    daily coding challenge 5

---
+++ Codewars/8kyu/8-basic-variable-assignment.js
@@ -0,0 +1,11 @@
+//URL -> https://www.codewars.com/kata/50ee6b0bdeab583673000025
+
+//* NAME -> Basic variable assignment
+
+//TODO INSTRUCTIONS -> This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+
+//SOLUTION(S) ->
+
+var a = 'code';
+var b = 'wa.rs';
+var name = a + b;
