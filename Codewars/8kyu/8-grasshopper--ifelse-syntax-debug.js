b1aec56a20170ba15aa69003222a159336e2780f
Sebastian Ospina <sc.ospc@gmail.com>
2022-05-09 22:42:21 -0500

    daily coding challenge 4

---
+++ Codewars/8kyu/8-grasshopper--ifelse-syntax-debug.js
@@ -0,0 +1,26 @@
+//URL -> https://www.codewars.com/kata/57089707fe2d01529f00024a
+
+//* NAME -> Grasshopper - If/else syntax debug
+
+//TODO INSTRUCTIONS -> While making a game, your partner, Greg, decided to create a function to check if the user is still alive called checkAlive/CheckAlive/check_alive. Unfortunately, Greg made some errors while creating the function.
+
+/*
+checkAlive/CheckAlive/check_alive should return true if the player's health is greater than 0 or false if it is 0 or below.
+
+The function receives one parameter health which will always be a whole number between -10 and 10.
+*/
+
+//SOLUTION(S) ->
+
+function checkAlive(health) {
+	if (health <= 0) {
+		return false;
+	} else {
+		return true;
+	}
+}
+
+//TEST CASES ->
+
+console.log(checkAlive(5), true);
+console.log(checkAlive(0), false);
