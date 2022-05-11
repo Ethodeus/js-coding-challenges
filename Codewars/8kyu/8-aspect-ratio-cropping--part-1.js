1b7885769385532aed2fc4974c13cf440fb29d8a
Sebastian Ospina <sc.ospc@gmail.com>
2022-05-10 20:58:52 -0500

    daily coding challenges 5

---
+++ Codewars/8kyu/8-aspect-ratio-cropping--part-1.js
@@ -0,0 +1,24 @@
+//URL -> https://www.codewars.com/kata/596e4ef7b61e25981200009f
+
+//* NAME -> Aspect Ratio Cropping - Part 1
+
+//TODO INSTRUCTIONS ->
+
+/*
+The aspect ratio of an image describes the proportional relationship between its width and its height. Most video shown on the internet uses a 16:9 aspect ratio, which means that for every pixel in the Y, there are roughly 1.77 pixels in the X (where 1.77 ~= 16/9). As an example, 1080p video with an aspect ratio of 16:9 would have an X resolution of 1920, however 1080p video with an aspect ratio of 4:3 would have an X resolution of 1440.
+
+Write a function that accepts arbitrary X and Y resolutions and converts them into resolutions with a 16:9 aspect ratio that maintain equal height. Round your answers up to the nearest integer.
+*/
+
+//SOLUTION(S) ->
+
+function aspectRatio(x, y) {
+	return [Math.ceil((y * 16) / 9), y];
+}
+
+//TEST CASES ->
+
+console.log(aspectRatio(640, 480), [854, 480], 'x = 640, y = 480');
+console.log(aspectRatio(960, 720), [1280, 720], 'x = 960, y = 720');
+console.log(aspectRatio(1440, 1080), [1920, 1080], 'x = 1440, y = 1080');
+console.log(aspectRatio(1920, 1440), [2560, 1440], 'x = 1920, y = 1440');
