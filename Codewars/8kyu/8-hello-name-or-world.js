e58f3539858e63f9d163fc5710c78e9fd6290268
Sebastian Ospina <sc.ospc@gmail.com>
2022-05-11 22:52:32 -0500

    daily coding challenge 9

--- Codewars/8kyu/8-hello-name-or-world.js
+++ Codewars/8kyu/8-hello-name-or-world.js
@@ -26,7 +26,3 @@ function hello(name) {
 console.log(hello('alice'), 'Hello, Alice!', "returns 'Hello, Alice!' when given 'alice'");
 console.log(hello(), 'Hello, World!', "returns 'Hello, World!' when name is not given");
 console.log(hello(''), 'Hello, World!', "returns 'Hello, World!' when name is an empty string");
-
-console.log(); // ->
-console.log(); // ->
-console.log(); // ->
