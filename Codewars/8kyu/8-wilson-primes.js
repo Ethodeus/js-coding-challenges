55c6fd372a376a7a600d8fbc8df89f22d38b8416
Sebastian Ospina <sc.ospc@gmail.com>
2022-05-14 01:30:09 -0500

    daily coding challenge 8

---
+++ Codewars/8kyu/8-wilson-primes.js
@@ -0,0 +1,32 @@
+//URL -> https://www.codewars.com/kata/55dc4520094bbaf50e0000cb
+
+//* NAME -> Wilson primes
+
+//TODO INSTRUCTIONS ->
+
+/*
+Wilson primes satisfy the following condition. Let P represent a prime number.
+
+Then ((P-1)! + 1) / (P * P) should give a whole number.
+
+Your task is to create a function that returns true if the given number is a Wilson prime.
+*/
+
+//SOLUTION(S) ->
+
+function factorial(n) {
+	if (n == 0) return 1;
+	else return n * factorial(n - 1);
+}
+
+function amIWilson(p) {
+	let result = (factorial(p - 1) + 1) / Math.pow(p, 2);
+	console.log(result);
+	return Number.isInteger(result);
+}
+
+//TEST CASES ->
+
+console.log(amIWilson(5), true);
+console.log(amIWilson(9), false);
+console.log(amIWilson(6), false);
