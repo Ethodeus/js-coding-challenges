865072bd19e8b336e697d66c7655527bb4a6c340
Sebastian Ospina <sc.ospc@gmail.com>
2022-05-19 23:13:17 -0500

    daily coding challenge 4

---
+++ Codewars/8kyu/8-compare-within-margin.js
@@ -0,0 +1,66 @@
+//URL -> https://www.codewars.com/kata/56453a12fcee9a6c4700009c
+
+//* NAME -> Compare within margin
+
+//TODO INSTRUCTIONS ->
+
+/*
+Create a function close_compare that accepts 3 parameters: a, b, and an optional margin. The function should return whether a is lower than, close to, or higher than b.
+
+a is considered "close to" b if margin is greater than or equal to the distance between a and b.
+
+Please note the following:
+
+When a is close to b, return 0.
+Otherwise...
+
+When a is less than b, return -1.
+
+When a is greater than b, return 1.
+
+If margin is not given, treat it as zero.
+
+Assume: margin >= 0
+
+Tip: Some languages have a way to make parameters optional.
+
+Example 1
+If a = 3, b = 5, and margin = 3, then close_compare(a, b, margin) should return 0.
+
+This is because a and b are no more than 3 numbers apart.
+
+Example 2
+If a = 3, b = 5, and margin = 0, then close_compare(a, b, margin) should return -1.
+
+This is because the distance between a and b is greater than 0, and a is less than b.
+*/
+
+//SOLUTION(S) ->
+
+function closeCompare(a, b, margin) {
+	if (!margin) {
+		//When a is lower than b, return -1.
+		if (a < b) return -1;
+		//When a is higher than b, return 1.
+		if (a > b) return 1;
+		// for a === b return should be 0.
+		if (a === b) return 0;
+	} else {
+		//When a is close to b, return 0 | close to ==> margin >= a - b
+		if (margin >= Math.abs(a - b) || a === b) return 0;
+		if (a < b) return -1;
+		if (a > b) return 1;
+	}
+
+	// margin >= 0, if not provided margin === 0
+}
+
+//TEST CASES ->
+
+console.log(closeCompare(4, 5), -1);
+console.log(closeCompare(5, 5), 0);
+console.log(closeCompare(6, 5), 1);
+console.log(closeCompare(-6, -5), -1);
+console.log(closeCompare(2, 5, 3), 0);
+console.log(closeCompare(8.1, 5, 3), 1);
+console.log(closeCompare(1.99, 5, 3), -1);
