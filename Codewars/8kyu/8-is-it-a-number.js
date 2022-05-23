0beda3ba7a910f76ba3f0a91d94cedde17d19ce0
Sebastian <sc.ospc@gmail.com>
2022-05-23 00:55:46 -0500

    daily coding challenge 3

---
+++ Codewars/8kyu/8-is-it-a-number.js
@@ -0,0 +1,35 @@
+//URL -> https://www.codewars.com/kata/57126304cdbf63c6770012bd
+
+//* NAME -> Is it a number?
+
+//TODO INSTRUCTIONS ->
+
+/*
+Given a string s, write a method (function) that will return true if its a valid single integer or floating number or false if its not.
+
+Valid examples, should return true:
+
+isDigit("3")
+isDigit("  3  ")
+isDigit("-3.23")
+should return false:
+
+isDigit("3-4")
+isDigit("  3   5")
+isDigit("3 5")
+isDigit("zero")
+*/
+
+//SOLUTION(S) ->
+
+function isDigit(s) {
+	return s == parseFloat(s);
+}
+
+//TEST CASES ->
+
+var input = "s2324";
+console.log(isDigit(input), false);
+
+input = "-234.4";
+console.log(isDigit(input), true);
