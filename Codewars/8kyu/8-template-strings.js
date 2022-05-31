5025d806a2bad02cdc5f347e9751e387159a9336
Sebastian <sc.ospc@gmail.com>
2022-05-30 19:42:26 -0500

    daily coding challenges 3

---
+++ Codewars/8kyu/8-template-strings.js
@@ -0,0 +1,30 @@
+//URL -> https://www.codewars.com/kata/55a14f75ceda999ced000048
+
+//* NAME -> Template Strings
+
+//TODO INSTRUCTIONS ->
+
+/*
+Template Strings
+
+Template Strings, this kata is mainly aimed at the new JS ES6 Update introducing Template Strings
+
+Task
+Your task is to return the correct string using the Template String Feature.
+
+Input
+Two Strings, no validation is needed.
+
+Output
+You must output a string containing the two strings with the word ```' are '```
+*/
+
+//SOLUTION(S) ->
+
+var TempleStrings = function (obj, feature) {
+	return `${obj} are ${feature}`;
+};
+
+//TEST CASES ->
+
+console.log(TempleStrings('Animals', 'Good'), 'Animals are Good');
