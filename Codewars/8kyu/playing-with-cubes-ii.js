ba3a57b3e815fe631d25046fccd6622e585e1c38
Sebastian <sc.ospc@gmail.com>
2022-07-23 16:10:28 -0500

    daily coding challenges 1

--- Codewars/8kyu/playing-with-cubes-ii.js
+++ Codewars/8kyu/playing-with-cubes-ii.js
@@ -28,3 +28,19 @@ class Cube {
 		this.side = Math.abs(n);
 	}
 }
+
+//Codewars Alternative ->
+
+class Cube {
+	constructor(side) {
+		this.setSide(side);
+	}
+
+	getSide() {
+		return this.side;
+	}
+
+	setSide(side = 0) {
+		this.side = Math.abs(side);
+	}
+}
