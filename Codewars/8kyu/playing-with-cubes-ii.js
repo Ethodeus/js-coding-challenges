81b591924c3874c02082de4442b922eca4135d73
Sebastian <sc.ospc@gmail.com>
2022-07-23 16:08:46 -0500

    daily coding challenges 1

---
+++ Codewars/8kyu/playing-with-cubes-ii.js
@@ -0,0 +1,36 @@
+//URL -> https://www.codewars.com/kata/55c0ac142326fdf18d0000af
+
+//* NAME -> Playing with cubes II
+
+//TODO INSTRUCTIONS ->
+
+/*
+Hey Codewarrior!
+
+You already implemented a Cube class, but now we need your help again! I'm talking about constructors. We don't have one. Let's code two: One taking an integer and one handling no given arguments!
+
+Also we got a problem with negative values. Correct the code so negative values will be switched to positive ones!
+
+The constructor taking no arguments should assign 0 to Cube's Side property.
+*/
+
+//SOLUTION(S) ->
+
+class Cube {
+	constructor(side) {
+		this.side = !side ? 0 : Math.abs(side); // if there is no argument, side will always be 0, and making sure the argument is always a positive number.
+	}
+
+	getSide() {
+		return this.side;
+	}
+	setSide(n) {
+		this.side = Math.abs(n);
+	}
+}
+
+//TEST CASES ->
+
+console.log();
+console.log();
+console.log();
