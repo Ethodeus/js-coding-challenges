1e1531ea67f1735bd9bd55f5fc08319e83fa2ad5
Sebastian <sc.ospc@gmail.com>
2022-07-24 15:27:24 -0500

    daily coding challenges 2

---
+++ Codewars/8kyu/string-templates--bug-fixing-5.js
@@ -0,0 +1,17 @@
+//URL -> https://www.codewars.com/kata/55c90cad4b0fe31a7200001f
+
+//* NAME -> String Templates - Bug Fixing #5
+
+//TODO INSTRUCTIONS -> Oh no! Timmy hasn't followed instructions very carefully and forgot how to use the new String Template feature, Help Timmy with his string template so it works as he expects!
+
+//SOLUTION(S) ->
+
+function buildString(...template) {
+	return `I like ${template.join(', ')}!`;
+}
+
+//TEST CASES ->
+
+console.log(buildString('Cheese', 'Milk', 'Chocolate'), 'I like Cheese, Milk, Chocolate!', 'Return the correct String');
+console.log(buildString('Cheese', 'Milk'), 'I like Cheese, Milk!', 'Return the correct String');
+console.log(buildString('Chocolate'), 'I like Chocolate!', 'Return the correct String');
