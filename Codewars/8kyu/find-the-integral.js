f7624abb84f6d5d9eec4823ed6327b467c4f17b7
Sebastian <sc.ospc@gmail.com>
2022-07-31 21:13:06 -0500

    daily coding challenges 4

---
+++ Codewars/8kyu/find-the-integral.js
@@ -0,0 +1,38 @@
+//URL -> https://www.codewars.com/kata/59811fd8a070625d4c000013
+
+//* NAME -> Find the Integral
+
+//TODO INSTRUCTIONS ->
+
+/*
+Create a function that finds the integral of the expression passed.
+
+In order to find the integral all you need to do is add one to the exponent (the second argument), and divide the coefficient (the first argument) by that new number.
+
+For example for 3x^2, the integral would be 1x^3: we added 1 to the exponent, and divided the coefficient by that new number).
+
+Notes:
+
+The output should be a string.
+The coefficient and exponent is always a positive integer.
+Examples
+ 3, 2  -->  "1x^3"
+12, 5  -->  "2x^6"
+20, 1  -->  "10x^2"
+40, 3  -->  "10x^4"
+90, 2  -->  "30x^3"
+*/
+
+//SOLUTION(S) ->
+
+function integrate(coefficient, exponent) {
+	return `${coefficient / (exponent + 1)}x^${exponent + 1}`;
+}
+
+//TEST CASES ->
+
+console.log(integrate(3,2), "1x^3");
+console.log(integrate(12,5), "2x^6");
+console.log(integrate(20,1), "10x^2")
+console.log(integrate(40,3), "10x^4")
+console.log(integrate(90,2), "30x^3")
