2860a3dd3e59b1635add10d79b34e87ab97bec77
Sebastian <sc.ospc@gmail.com>
2022-07-31 20:49:43 -0500

    daily coding challenges 1

---
+++ Codewars/8kyu/5-without-numbers.js
@@ -0,0 +1,20 @@
+//URL -> https://www.codewars.com/kata/59441520102eaa25260000bf
+
+//* NAME -> 5 without numbers !!
+
+//TODO INSTRUCTIONS ->
+
+/*
+Write a function that always returns 5
+
+Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+Good luck :)
+*/
+
+//SOLUTION(S) ->
+
+function unusualFive() {
+	let str = 'abcde';
+	return str.length;
+}
