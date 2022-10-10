8b53fd5b1886c30f90b55c2caf57715c696f73f1
Sebastian <sc.ospc@gmail.com>
2022-10-09 21:20:06 -0500

    type of sum code challenge

---
+++ Codewars/8kyu/type-of-sum.js
@@ -0,0 +1,26 @@
+//URL -> https://www.codewars.com/kata/5a2e9ae2b6cfd7692a0000ba
+
+//* NAME -> Type of sum
+
+//TODO INSTRUCTIONS -> Return the type of the sum of the two arguments
+
+//SOLUTION(S) ->
+
+function typeOfSum(a, b) {
+	return typeof (a + b);
+}
+
+//TEST CASES ->
+
+console.log(typeOfSum(12, 1), 'number');
+console.log(typeOfSum('d', 1), 'string');
+console.log(typeOfSum(1, 'a'), 'string');
+console.log(typeOfSum('dd', ''), 'string');
+console.log(typeOfSum(true, 1), 'number');
+console.log(typeOfSum('s', false), 'string');
+console.log(typeOfSum(null, 1), 'number');
+console.log(typeOfSum('s', null), 'string');
+console.log(typeOfSum(null, undefined), 'number');
+console.log(typeOfSum(undefined, 're'), 'string');
+console.log(typeOfSum(undefined, true), 'number');
+console.log(typeOfSum(null, false), 'number');
